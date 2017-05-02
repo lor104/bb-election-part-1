9e76030930df85f7a32c70e69ae8201e6346286a
lor104 <lauren.rosentzveig@gmail.com>
2017-05-02 14:05:14 -0400

    finisf bikini bottom election part 1

--- election.js
+++ election.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
 
-  // Imagination!
+  $.ajax({
+    url: 'https://bb-election-api.herokuapp.com/',
+    method: 'GET',
+    dataType: 'json'
+  }).done(function(data) {
+    for (var i = 0; i < data.candidates.length; i++) {
+      candidate = data.candidates[i]
+
+      cName = candidate.name
+      cVotes = candidate.votes
+
+      cInfo = cName + ' has ' + cVotes + ' votes.'
+
+    $('<li>').html(cInfo).appendTo('#candidates');
+  }
+  });
 
 });
